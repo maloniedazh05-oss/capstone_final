4ba27ecf3531a8215a877220aada2d4322bf2427
maloniedev-cp <maloniedev@gmail.com>
2026-09-09 10:16:15 +0800

    run file added

--- run.bat
+++ run.bat
@@ -1,22 +1,27 @@
 @echo off
-REM 
+
 call venv\scripts\activate
 
-REM  
+
 pip install --upgrade pip
 if not exist venv\Lib\site-packages\pandas (pip install pandas)
 if not exist venv\Lib\site-packages\flask (pip install flask)
 if not exist venv\Lib\site-packages\statsmodels (pip install statsmodels)
 
-REM
 
 py_backend\forecasting.py
 
 REM Check if node_modules folder exists
+if not exist asset (
+    mkdir asset
+)
+
 if not exist asset\node_modules (
     REM If not, install fontawesome
     echo Installing fontawesome...
+    cd asset
     npm install @fortawesome/fontawesome-free
+    cd ..
 ) else (
     REM If yes, check if npm is installed
     where npm >nul 2>nul
@@ -26,5 +31,4 @@ if not exist asset\node_modules (
     )
 )
 
-REM 
 start http://localhost/capstone_final
